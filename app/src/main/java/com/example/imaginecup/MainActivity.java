fc62134fb187bced32f2b15e97c07f78a5d719ad
ming-taro <hera002008@naver.com>
2023-01-03 23:49:11 +0900

    [ADD] 달력 페이지 생성

--- app/src/main/java/com/example/imaginecup/MainActivity.java
+++ app/src/main/java/com/example/imaginecup/MainActivity.java
@@ -1,14 +1,57 @@
 package com.example.imaginecup;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MaterialCalendarView calendarView;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_analysis);
+
+        calendarView = findViewById(R.id.calendar_view);
+
+        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.months)));
+        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.weekdays)));
+
+        calendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader);
+
+        calendarView.addDecorators(new DayDecorator(this));
+    }
+
+    private static class DayDecorator implements DayViewDecorator{
+
+        private final Drawable drawable;
+
+        private DayDecorator(Context context) {
+            drawable = ContextCompat.getDrawable(context, R.drawable.calendar_selector);
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return true;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setSelectionDrawable(drawable);
+        }
     }
 }
+

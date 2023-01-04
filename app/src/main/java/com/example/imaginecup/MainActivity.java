96814bd51b30344ccc149f690715ff1ce587e7ae
ming-taro <hera002008@naver.com>
2023-01-04 11:40:40 +0900

    [ADD] 달력 페이지 dialog로 변경, 스트레스 지수 막대그래프 추가

--- app/src/main/java/com/example/imaginecup/MainActivity.java
+++ app/src/main/java/com/example/imaginecup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.imaginecup;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -7,7 +8,18 @@ import androidx.core.content.ContextCompat;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.ImageView;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
@@ -15,43 +27,65 @@ import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
 import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    private MaterialCalendarView calendarView;
+    private Context context;
+    private ImageView ivCalender;
+    private BarChart chartStressLevel;
+    private CalendarActivity calendarActivity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_analysis);
+        context = this;
 
-        calendarView = findViewById(R.id.calendar_view);
+        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams();
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+        layoutParams.dimAmount = 0.8f;
+        getWindow().setAttributes(layoutParams);
 
-        calendarView.setTitleFormatter(new MonthArrayTitleFormatter(getResources().getTextArray(R.array.months)));
-        calendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(getResources().getTextArray(R.array.weekdays)));
+        ivCalender = findViewById(R.id.imageview_calendar);
 
-        calendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader);
+        ivCalender.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendarActivity = new CalendarActivity(context, "test");
+                calendarActivity.show();
+            }
+        });
 
-        calendarView.addDecorators(new DayDecorator(this));
+        chartStressLevel = findViewById(R.id.chart_stress_index);
+        initChart();
+        setChartData();
     }
 
-    private static class DayDecorator implements DayViewDecorator{
+    private void initChart(){
+        chartStressLevel.setDrawGridBackground(false);
+        chartStressLevel.setDrawBarShadow(false);
+        chartStressLevel.setDrawBorders(false);
 
-        private final Drawable drawable;
+        Description description = new Description();
+        chartStressLevel.setDescription(description);
 
-        private DayDecorator(Context context) {
-            drawable = ContextCompat.getDrawable(context, R.drawable.calendar_selector);
+        chartStressLevel.animateX(1000);
+        chartStressLevel.animateY(1000);
+    }
+
+    private void setChartData(){
+        chartStressLevel.setScaleEnabled(false);
+        ArrayList levelList = new ArrayList<>();
+
+        for(int index = 0; index < 5; index++){
+            levelList.add(new BarEntry(index, index));
         }
 
-        @Override
-        public boolean shouldDecorate(CalendarDay day) {
-            return true;
-        }
-
-        @Override
-        public void decorate(DayViewFacade view) {
-            view.setSelectionDrawable(drawable);
-        }
+        BarDataSet barDataSet = new BarDataSet(levelList, "stress level");
+        BarData barData = new BarData(barDataSet);
+        chartStressLevel.setData(barData);
     }
 }
 

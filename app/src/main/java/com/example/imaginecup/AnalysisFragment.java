f07365717f53b0c330091e559ffdb41714d55292
ming-taro <hera002008@naver.com>
2023-01-05 20:01:39 +0900

    [ADD] 분노 지수 그래프 UI수정(상단 부분 둥글게, 색깔 변경, 테두리 음영 효과)

--- app/src/main/java/com/example/imaginecup/AnalysisFragment.java
+++ app/src/main/java/com/example/imaginecup/AnalysisFragment.java
@@ -1,6 +1,7 @@
 package com.example.imaginecup;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,23 +15,30 @@ import androidx.fragment.app.Fragment;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.dataprovider.BarDataProvider;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class AnalysisFragment extends Fragment {
 
     private Context context;
     private ImageView ivCalender;
-    private BarChart chartStressLevel;
-    private CalendarActivity calendarActivity;
+    private BarChart chartAngerLevel;
+    private CalendarDialog calendarDialog;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_analysis, container, false);
+        View view = inflater.inflate(R.layout.fragment_analysis, container, false);
 
         context = container.getContext();
 
@@ -39,41 +47,93 @@ public class AnalysisFragment extends Fragment {
         ivCalender.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                calendarActivity = new CalendarActivity(context, "test");
-                calendarActivity.show();
+                calendarDialog = CalendarDialog.newInstance();
+                calendarDialog.show(getChildFragmentManager(), CalendarDialog.TAG_EVENT_DIALOG);
             }
         });
 
-        chartStressLevel = view.findViewById(R.id.chart_stress_index);
+        chartAngerLevel = view.findViewById(R.id.chart_stress_index);
         initChart();
         setChartData();
 
         return view;
     }
 
+    private void setAxis(){
+        XAxis xAxis = chartAngerLevel.getXAxis();          //차트 뒷배경 그리드 삭제
+        xAxis.setDrawAxisLine(false);
+        xAxis.setDrawGridLines(false);
+
+        YAxis axisLeft = chartAngerLevel.getAxisLeft();
+        axisLeft.setDrawAxisLine(false);
+        axisLeft.setDrawGridLines(false);
+
+        YAxis axisRight = chartAngerLevel.getAxisRight();
+        axisRight.setDrawAxisLine(false);
+        axisRight.setDrawGridLines(false);
+
+        ArrayList<String> time = new ArrayList<>();
+
+        for(int index = 0; index < 5; index++){
+            time.add(Integer.toString(index));
+        }
+
+        xAxis.isEnabled();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);             //x축 데이터 아래에 표시
+        xAxis.setGranularity(1f);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(time));
+
+//        ArrayList<String> level = new ArrayList<>();
+//        level.add("Low");
+//        level.add("High");
+
+//        axisLeft.setValueFormatter(new IndexAxisValueFormatter(level));
+
+        axisLeft.setDrawLabels(false);     //왼쪽 데이터 라벨 삭제
+        axisRight.setDrawLabels(false);    //오른쪽 데이터 라벨 삭제
+
+        chartAngerLevel.getLegend().setEnabled(false);  //차트 범례 삭제
+    }
+
     private void initChart(){
-        chartStressLevel.setDrawGridBackground(false);
-        chartStressLevel.setDrawBarShadow(false);
-        chartStressLevel.setDrawBorders(false);
+        chartAngerLevel.setDrawGridBackground(false);
+        chartAngerLevel.setDrawBarShadow(false);
+        chartAngerLevel.setDrawBorders(false);
 
-        Description description = new Description();
-        chartStressLevel.setDescription(description);
+        chartAngerLevel.getDescription().setEnabled(false);
+        chartAngerLevel.setTouchEnabled(false);
+        chartAngerLevel.setBackgroundColor(Color.WHITE);
 
-        chartStressLevel.animateX(1000);
-        chartStressLevel.animateY(1000);
+        setAxis();
+
+        chartAngerLevel.animateX(1000);        //차트 애니메이션
+        chartAngerLevel.animateY(1000);
+
+        CustomBarChartRender barChartRender = new CustomBarChartRender(chartAngerLevel, chartAngerLevel.getAnimator(),
+                chartAngerLevel.getViewPortHandler());
+
+        barChartRender.setRadius(50);  //그래프 상단 round 효과
+        chartAngerLevel.setRenderer(barChartRender);
     }
 
     private void setChartData(){
-        chartStressLevel.setScaleEnabled(false);
-        ArrayList levelList = new ArrayList<>();
+        chartAngerLevel.setScaleEnabled(false);
+        ArrayList<BarEntry> levelList = new ArrayList<>();
 
-        for(int index = 0; index < 5; index++){
-            levelList.add(new BarEntry(index, index));
-        }
+        levelList.add(new BarEntry(0, 3));
+        levelList.add(new BarEntry(1, 1));
+        levelList.add(new BarEntry(2, 4));
+        levelList.add(new BarEntry(3, 2));
+        levelList.add(new BarEntry(4, 1));
 
         BarDataSet barDataSet = new BarDataSet(levelList, "stress level");
+        barDataSet.setGradientColor(Color.parseColor("#00FF5722"), Color.parseColor("#FFE11313")); //그래프 색깔
+        barDataSet.setDrawValues(false);
+
         BarData barData = new BarData(barDataSet);
-        chartStressLevel.setData(barData);
+        barData.setBarWidth(0.5f);   //그래프 너비
+
+        chartAngerLevel.setData(barData);
     }
 
 }
---
+++ app/src/main/java/com/example/imaginecup/CalendarDialog.java
@@ -0,0 +1,117 @@
+package com.example.imaginecup;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.PopupWindow;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.DayViewDecorator;
+import com.prolificinteractive.materialcalendarview.DayViewFacade;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter;
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter;
+
+public class CalendarDialog extends DialogFragment {
+
+    private MaterialCalendarView materialCalendarView;
+    public static final String TAG_EVENT_DIALOG = "dialog_event";
+    private Context context;
+
+    public static CalendarDialog newInstance(){
+
+        CalendarDialog calendarDialog = new CalendarDialog();
+
+        return calendarDialog;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.dialog_calendar, container, false);
+
+        context = view.getContext();
+        initDialog(view);
+
+        return view;
+    }
+
+    private void dismissDialog(){
+        this.dismiss();
+    }
+
+    private void initDialog(View view){
+
+        materialCalendarView = view.findViewById(R.id.calendar_view);
+
+        materialCalendarView.setTitleFormatter(new MonthArrayTitleFormatter(context.getResources().getTextArray(R.array.months)));
+        materialCalendarView.setWeekDayFormatter(new ArrayWeekDayFormatter(context.getResources().getTextArray(R.array.weekdays)));
+
+        materialCalendarView.setHeaderTextAppearance(R.style.CalendarWidgetHeader);
+
+        materialCalendarView.addDecorators(new CalendarDialog.DayDecorator(context));
+
+        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
+            @Override
+            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
+                dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        Window window = getDialog().getWindow();
+        if(window == null) return;
+
+        WindowManager.LayoutParams params = window.getAttributes();
+
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+
+        window.setAttributes(params);
+//        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));  //fragment 기본 padding 삭제
+        window.setGravity(Gravity.BOTTOM);
+    }
+
+    private static class DayDecorator implements DayViewDecorator {
+
+        private final Drawable drawable;
+
+        private DayDecorator(Context context) {
+            drawable = ContextCompat.getDrawable(context, R.drawable.calendar_selector);
+        }
+
+        @Override
+        public boolean shouldDecorate(CalendarDay day) {
+            return true;
+        }
+
+        @Override
+        public void decorate(DayViewFacade view) {
+            view.setSelectionDrawable(drawable);
+        }
+    }
+}
